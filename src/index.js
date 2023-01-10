45b0d02016bd3b4a7499e0778fbbbcc26cfb33bd
Space Pocket <space1985pocket@gmail.com>
2023-01-10 09:14:04 +0300

    init: my solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let j = 0;
+    let arr = str.split('');
+    let check1 = (el1, el2, index, arr) => {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (arr[index] == bracketsConfig[i][0] && arr[index+1] == bracketsConfig[i][1]) {
+
+                arr.splice(index, 2);
+                console.log(arr);
+                index=0;
+                i=-1;
+                
+            }    
+         
+        }
+        j=index+1
+    }
+
+    while (j < arr.length - 1) {
+        check1(arr[j], arr[j + 1], j, arr);
+    }
+
+    result = (arr.length == 0)?(true):(false)
+    
+    return result
 }
