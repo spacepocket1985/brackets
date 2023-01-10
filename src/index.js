fbb008fbf482a98d7df33178314c43d140e83413
Space Pocket <space1985pocket@gmail.com>
2023-01-10 12:38:05 +0300

    refactor: delete console.log

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-let j = 0;
+    let j = 0;
     let arr = str.split('');
     let check1 = (el1, el2, index, arr) => {
         for (let i = 0; i < bracketsConfig.length; i++) {
-            if (arr[index] == bracketsConfig[i][0] && arr[index+1] == bracketsConfig[i][1]) {
+            if (arr[index] == bracketsConfig[i][0] && arr[index + 1] == bracketsConfig[i][1]) {
 
                 arr.splice(index, 2);
-                console.log(arr);
-                index=0;
-                i=-1;
-                
-            }    
-         
+                index = 0;
+                i = -1;
+            }
         }
-        j=index+1
+        j = index + 1
     }
 
     while (j < arr.length - 1) {
